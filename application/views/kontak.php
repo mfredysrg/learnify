9c5de2def42685abe4d1a2856ebec8c1d5c30ac3
mfredysrg <mfredyansyahsiregar@gmail.com>
2024-09-17 15:45:21 +0700

    First Commit

--- application/views/kontak.php
+++ application/views/kontak.php
@@ -19,26 +19,26 @@
 <!--================Contact Area =================-->
 <section class="contact_area p_40">
     <div class="container">
-        <div style="width: 100%"><iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=SMKN%201%20CIAMIS+(Learnify)&amp;ie=UTF8&amp;t=&amp;z=18&amp;iwloc=A&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">gps coordinates</a></iframe></div><br />
+        <div style="width: 100%"><iframe width="100%" height="600" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d632.5699082552746!2d97.14758391125235!3d5.182362857209546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304783fa083161ad%3A0x2eb792dabeee9c8f!2sProgram%20Pascasarjana%20Universitas%20Malikussaleh%20Lhokseumawe!5e0!3m2!1sid!2sid!4v1725953634430!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">gps coordinates</a></iframe></div><br />
         <div class="row">
             <div class="col-lg-9">
                 <div class="contact_info">
                     <div class="info_item">
                         <i class="lnr lnr-home"></i>
-                        <h6>Jl.Jendral Soedirman No.269
-                            Ciamis, Jawa Barat</h6>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <h6>Jl. Teungku Chik Ditiro No.26, Lancang Garam, Kec. Muara Dua
+                            Kota Lhokseumawe, Aceh 24352</h6>
+                        <p>Alamat Kantor</p>
                     </div>
                     <div class="info_item">
                         <i class="lnr lnr-phone-handset"></i>
                         <h6><a href="#">
-                                (0265) 771204</a></h6>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            +62 811 6840 024</a></h6>
+                        <p>Nomor Telepon</p>
                     </div>
                     <div class="info_item">
                         <i class="lnr lnr-envelope"></i>
-                        <h6><a href="#">apps.learnify@gmail.com</a></h6>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <h6><a href="#">superteamacademi@gmail.com</a></h6>
+                        <p>Email</p>
                     </div>
                 </div>
             </div>
